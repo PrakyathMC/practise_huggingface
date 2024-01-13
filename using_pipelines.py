b09c66f9fac6d5ff42bf995625bb200bbfb8eb8c
Prakyath M.C <prakyathmc7@gmail.com>
2024-01-13 00:16:56 +0000

    Initial Commit

---
+++ using_pipelines.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+classifier = pipeline("sentiment-analysis")
+
+res = classifier("I'vs been waiting for a huggingface course my whole life!!!")
+
+print(res)
